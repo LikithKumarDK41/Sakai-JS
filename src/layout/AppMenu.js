c0fdf76582738a8827728b16e9b7391e85e57e04
LikithKumarDK41 <likithkumar.dk@nichi.com>
2024-05-17 18:07:00 +0530

    Fix: Clean up

--- src/layout/AppMenu.js
+++ src/layout/AppMenu.js
@@ -5,21 +5,19 @@ import { BsHouseGearFill } from "react-icons/bs";
 import AppMenuitem from '@/layout/AppMenuitem';
 import { LayoutContext } from '@/layout/context/layoutcontext';
 import { MenuProvider } from '@/layout/context/menucontext';
-import { getValueByKeyRecursively as translate } from '@/helper';
 
 const AppMenu = () => {
-    const { localeJson } = useContext(LayoutContext);
     const url = window.location.pathname;
 
     // Admin side bar information
     const adminModel = [
         {
-            label: translate(localeJson, 'event_information_'),
+            label: "Dashboard",
             icon: <MdSettings size={16} />,
             class: "without-top-element",
             items: [
                 {
-                    label: translate(localeJson, 'event_status_list'),
+                    label: "Dashboard",
                     icon: <BsHouseGearFill size={16} />,
                 },
             ]

7d5daa60d037b75b7e25ea8125d96520d7bff740
LikithKumarDK41 <likithkumar.dk@nichi.com>
2024-05-22 16:44:14 +0530

    Update: Layoutsetup

--- layout/AppTopbar.js
+++ layout/AppTopbar.js
@@ -4,36 +4,35 @@ import React, {
     useImperativeHandle,
     useRef,
 } from "react";
-import { useRouter } from "next/navigation";
 import _ from "lodash";
+import { FiMenu, FiShoppingCart, FiUser } from 'react-icons/fi';
+import Image from 'next/image';
 
 import { LayoutContext } from "@/layout/context/layoutcontext";
-import { ImageComponent, DateTimeDisplay } from "@/components";
 
 const AppTopbar = forwardRef((props, ref) => {
     const {
-        layoutConfig,
         onMenuToggle,
     } = useContext(LayoutContext);
-    const router = useRouter();
 
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
-    const topbarmenubuttonRef = useRef(null);
 
     useImperativeHandle(ref, () => ({
         menubutton: menubuttonRef.current,
         topbarmenu: topbarmenuRef.current,
-        topbarmenubutton: topbarmenubuttonRef.current,
     }));
 
     /**
      * App top bar right view
      */
     const topBarRight = (
-        <div ref={topbarmenuRef} className="header-details-second">
-            <div className='header-details-second-date-time-picker mr-3'>
-                <DateTimeDisplay />
+        <div ref={topbarmenuRef} className="right-icons">
+            <div class="cart-icon">
+                <FiShoppingCart />
+            </div>
+            <div class="profile-icon">
+                <FiUser />
             </div>
         </div>
     );
@@ -41,65 +40,21 @@ const AppTopbar = forwardRef((props, ref) => {
     return (
         <React.Fragment>
             <div className="layout-topbar">
-                {layoutConfig.menuMode === "static" && (
-                    <div className="logo-details">
-                        <div className="logo-view cursor-pointer">
-                            <div className="hamburger px-2">
-                                <button
-                                    ref={menubuttonRef}
-                                    type="button"
-                                    className="p-link layout-topbar-button"
-                                    onClick={onMenuToggle}
-                                >
-                                    <i className="pi pi-bars" />
-                                </button>
-                            </div>
-                            <ImageComponent
-                                imageProps={{
-                                    src: ``,
-                                    width: 220,
-                                    height: 45,
-                                    alt: "logo",
-                                    text: "テレネット",
-                                    onClick: () => {
-                                        router.push("/");
-                                    },
-                                }}
-                            />
-                        </div>
-                    </div>
-                )}
-                <div
-                    className="header-details"
-                >
-                    {layoutConfig.menuMode === "static" && (
-                        <div className="hamburger">
-                            <button
-                                ref={menubuttonRef}
-                                type="button"
-                                className="p-link layout-topbar-button"
-                                onClick={onMenuToggle}
-                            >
-                                <i className="pi pi-bars" />
-                            </button>
-                        </div>
-                    )}
-                    <div
-                        className={`header-details-first-view`}
-                    >
-                        <div
-                            title={"Header"}
-                            className="header-details-first flex flex-column"
+                <div class="left-icons">
+                    <div class="hamburger-icon">
+                        <button
+                            ref={menubuttonRef}
+                            type="button"
+                            onClick={onMenuToggle}
                         >
-                            <div
-                                className={`text-base white-space-nowrap overflow-hidden text-overflow-ellipsis cursor-pointer`}
-                            >
-                                Header
-                            </div>
-                        </div>
-                        {topBarRight}
+                            <FiMenu />
+                        </button>
+                    </div>
+                    <div class="logo-container">
+                        <Image src="/layout/images/mobile-logo.png" alt="Logo" width={100} height={100} />
                     </div>
                 </div>
+                {topBarRight}
             </div>
         </React.Fragment>
     );
